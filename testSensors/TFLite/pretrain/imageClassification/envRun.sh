5ddde8a5102c196451022c96e98ad4da4c266d03
Michael Ly <Michael.QV.Ly@Gmail.com>
2021-11-20 18:34:19 -0600

    Add script to run virtual environment for image classification

---
+++ testSensors/TFLite/pretrain/imageClassification/envRun.sh
@@ -0,0 +1,5 @@
+printf "Launching image classification virtual environment\n"
+
+. ~/Git/EE4201/testSensors/TFLite/pretrain/imageClassification/venvImageClass/bin/activate
+
+printf "Image classification virtual environment launched\n"
