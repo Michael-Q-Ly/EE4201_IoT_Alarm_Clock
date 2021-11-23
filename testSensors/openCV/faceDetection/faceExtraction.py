230f5e35d2c67686bb14e992e7db09fe5198175d
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-11-23 01:46:31 -0600

    Add border around face images

--- testSensors/openCV/faceDetection/faceExtraction.py
+++ testSensors/openCV/faceDetection/faceExtraction.py
@@ -1,5 +1,8 @@
-from PIL import Image
+from os import read
+from PIL import Image, ImageDraw
 import face_recognition as fr
+
+borderColor = (255,0,0)
 
 # Load the jpg file into a numpy array
 image = fr.load_image_file("/home/pi32/Git/EE4201/Face_Recog/Face_Images/Michael/01.jpg")
@@ -8,15 +11,15 @@ face_locations = fr.face_locations(image)
 
 print( "I found {} face(s) in this photograph.".format( len(face_locations) ) )
 
+pil_image   = Image.fromarray(image)
+draw        = ImageDraw.Draw(pil_image)
+
+
 for face_location in face_locations:
     # Print the location of each face in this image
     top, right, bottom, left = face_location
     print( "A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format( top, left, bottom, right ) )
 
-    face_image  = image[top:bottom, left:right]
-    pil_image   = Image.fromarray(face_image)
-    pil_image.show()
+    draw.rectangle( ((left, top), (right, bottom)), outline = borderColor )
 
-    face_image  = image[ top:bottom, left:right ]
-    pil_image   = Image.fromarray(face_image)
-    pil_image.show()
+pil_image.show()
